1b8b3acb49831f43acf5c5fe26becd7873429752
Camila-Moura <camila.miranda.moura@gmail.com>
2020-05-08 10:43:38 -0300

    Interpretação de código - Ex. 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,6 @@
+// INTERPRETAÇÃO DE CÓDIGO 
+ /* 1. A função, quando chamada, executa um prompt que pergunta ao usuário qual a cotação do dólar no dia e
+ retorna o parâmetro (valor em dólar a ser convertido) multiplicado pela cotação. A variável meuDinheiro chama
+ a função e lhe dá parâmetro de 100. Por último, a variável é impressa no console. */
+
+ /* 2.  */
