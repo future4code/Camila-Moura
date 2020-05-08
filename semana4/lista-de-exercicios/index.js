378d6860cb288cda690cc588a1aa59f982bad5e9
Camila-Moura <camila.miranda.moura@gmail.com>
2020-05-08 10:58:56 -0300

    Interpretação de código - Ex. 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -3,4 +3,10 @@
  retorna o parâmetro (valor em dólar a ser convertido) multiplicado pela cotação. A variável meuDinheiro chama
  a função e lhe dá parâmetro de 100. Por último, a variável é impressa no console. */
 
- /* 2.  */
+ /* 2. A função possui dois parâmetros, tipoDeInvestimento e valor. Dentro dela, é criada uma nova variável. 
+ O switch executará um case dependendo do valor atribuído ao parâmetro tipoDeInvestimento quando a função for 
+ chamada. Em cada um dos cases, dentro do switch, a variável valorAposInvestimento é igualada ao parâmetro valor 
+ multiplicado por um número. Caso o parâmetro não esteja previsto no switch, ele gerará um alert para o usuário.
+ O return da função é a variável (ou seja, o valor multiplicado pelo investimento). Depois, a função é chamada
+ duas vezes, com diferentes parâmetros. As chamadas da função são impresas no console. No console, apareceram os 
+ números 165 (novoMontante) e no caso da segunda chamada, aparecerá o alert.*/
