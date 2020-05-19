2bfae323f29deb4bfa83df5f1c618c179e8d8221
Camila-Moura <camila.miranda.moura@gmail.com>
2020-05-19 17:26:19 -0300

    Exercício LocalStorage

---
+++ semana6/boiler-plate/build/precache-manifest.b8190517d6e61507f8ac2f4e6c5b6673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327444df8cd207d599aa76226ad6855a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "465c7bde2738e8ead0fa",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "078d8750ac3017505296",
+    "url": "/static/js/2.29e9a946.chunk.js"
+  },
+  {
+    "revision": "465c7bde2738e8ead0fa",
+    "url": "/static/js/main.cfc7401a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
